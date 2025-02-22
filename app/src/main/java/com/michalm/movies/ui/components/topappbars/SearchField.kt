65396c7f0d4acf674f33fa74679a7db731a0d775
Michal Matczak <matczak.wln@gmail.com>
2025-02-22 14:56:56 +0100

    Feat(UI): Automatically request focus for search text field

--- app/src/main/java/com/michalm/movies/ui/components/topappbars/SearchField.kt
+++ app/src/main/java/com/michalm/movies/ui/components/topappbars/SearchField.kt
@@ -5,7 +5,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import com.michalm.movies.R
 import com.michalm.movies.ui.MovieViewModel
@@ -17,8 +21,14 @@ fun SearchField(
     viewModel: MovieViewModel,
     onValueChange: (String) -> Unit
 ) {
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     TextField(
-        modifier = modifier,
+        modifier = modifier
+            .focusRequester(focusRequester),
         value = query,
         onValueChange = {
             onValueChange(it)
